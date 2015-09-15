9bdaf43ed3d64a02d4c6e7e1ea5aa860ce7f27c4
vmolsa <ville.molsa@gmail.com>
2015-09-15 17:09:02 +0300

    Add binary

---
+++ bin/sha1-file.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var sha1File = require('../index.js');
+var files = process.argv;
+
+files.splice(0, 1);
+files.splice(0, 1);
+
+if (files.length) {
+  files.forEach(function(file) {
+    if (fs.existsSync(file)) {
+      console.log(sha1File(file));
+    }
+  }
+}
